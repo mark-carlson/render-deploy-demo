f4d6e044964c4e6884a69894f59652e4a17ff762
Mark Carlson <mcarlson@avenuecode.com>
2024-06-10 20:36:27 -0700

    Adds port path

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
+
+app.get("/port", (req, res) => res.json(`The port is ${PORT}`));
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
